ab97fd71770e22664214211d0c0425b7ca255226
Ali Hassan <alihassan3522966@gmail.com>
2022-06-26 16:00:40 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/weatherapprepositry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weatherapprepositry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:weatherapp/screens/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
